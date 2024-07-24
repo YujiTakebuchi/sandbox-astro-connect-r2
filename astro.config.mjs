add9a772adcac17faa332f7ad798a394be478f46
YujiTakebuchi <takeyou.swift@gmail.com>
2024-07-24 16:55:36 +0900

    R2連携テスト

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+      configPath: "wrangler.toml",
+    },
+  }),
   server: { port: 8080 },
 });
